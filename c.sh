8414705410b7a283b978fe1720d761aec892b029
shasabbir <shasabbir234@gmail.com>
2022-08-13 00:46:41 +0600

    cffxx

--- c.sh
+++ c.sh
@@ -1,5 +1,10 @@
 echo "Enter your message"
 read message
+while [ -z "$message" ]
+do
+echo "You must enter a message"
+read message
+done
 git checkout -b sabbir
 git add .
 git commit -m"${message}"
